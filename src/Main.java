f13b29ed964e7b99ff5002c958cea00e1efc934a
zbilgeozkan <zeynepbilgeozkan@hotmail.com>
2022-12-29 04:09:38 +0300

    Code added.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 /* Not Ortalaması Hesaplayan Program
+
 Java ile Matematik, Fizik, Kimya, Türkçe, Tarih, Müzik derslerinin
 sınav puanlarını kullanıcıdan alan ve ortalamalarını hesaplayıp ekrana bastırılan programı yazın.
 
